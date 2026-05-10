67d4a5f18e3d65539bd24fc88cbba1d81b141129
Vesselin Tsvetanov <vcvetanov@yahoo.com>
2026-05-10 16:18:08 +0200

    further refinement of the used toolchain

---
+++ scripts/update_rc.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+from jinja2 import Environment
+from jinja2 import FileSystemLoader
+from typing import Generator
+
+# Note:
+# Intentionally all relevant variables are set as global variables
+
+# fmt: off
+TEMPLATE_DIR  = Path(__file__).parent.parent / "{{cookiecutter.project_name}}" / "templates"
+TEMPLATE_NAME = "resources.qrc"
+ICONS_DIR     = TEMPLATE_DIR.parent / "gui" / "icons"
+RC_FILE       = ICONS_DIR.parent / TEMPLATE_NAME
+# fmt: on
+
+
+def list_all_icons() -> Generator[Path, None, None]:
+    yield from (file for file in ICONS_DIR.iterdir() if file.is_file())
+
+
+def write_resource_file() -> None:
+    # fmt: off
+    env      = Environment(
+        loader        = FileSystemLoader(TEMPLATE_DIR),
+        lstrip_blocks = True, # removes the spaces left from the comments at the start
+    )
+    template = env.get_template(TEMPLATE_NAME)
+    # fmt: on
+
+    with open(RC_FILE, "w") as f:
+        icons = list_all_icons()
+        ready = template.render(icons=icons)
+        f.write(ready.strip())
+
+
+if __name__ == "__main__":
+    write_resource_file()
