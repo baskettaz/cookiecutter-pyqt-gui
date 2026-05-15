2b282e26d3a22263293dd5c21f190a0beb6a16de
Vesselin Tsvetanov <vcvetanov@yahoo.com>
2026-05-15 12:42:48 +0200

    changes, not funcitonal

--- {{cookiecutter.project_name}}/scripts/update_rc.py
+++ {{cookiecutter.project_name}}/scripts/update_rc.py
@@ -9,7 +9,9 @@ from typing import Generator
 # fmt: off
 TEMPLATE_DIR  = Path(__file__).parent.parent / "{{cookiecutter.package_name}}" / "templates"
 TEMPLATE_NAME = "resources.qrc"
-ICONS_DIR     = TEMPLATE_DIR.parent / "gui" / "icons"
+print(f"---> {TEMPLATE_DIR}")
+# ICONS_DIR     = TEMPLATE_DIR.parent / "gui" / "icons"
+ICONS_DIR     = "."
 RC_FILE       = ICONS_DIR.parent / TEMPLATE_NAME
 # fmt: on
 
